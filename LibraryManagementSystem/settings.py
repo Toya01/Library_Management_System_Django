2193865edacaff3fff7d811baa5fa844d1c2c4f9
Toya <119118438+Toya01@users.noreply.github.com>
2025-05-29 10:12:50 +0545

    Update settings.py

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -122,5 +122,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'toyachauhary976768@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'nrhhpfernzxnuibf'  # Your Gmail app password
+EMAIL_HOST_USER = ''  # Your Gmail address
+EMAIL_HOST_PASSWORD = ''  # Your Gmail app password
